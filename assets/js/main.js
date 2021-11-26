864085f2e75ea60d5d041a4fe32651b1b5effc99
Eduardo Gomes <78993701+duardobgf@users.noreply.github.com>
2021-11-25 22:01:44 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,22 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/* ----- Ver Mais -----*/
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for(i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click",function(){
+        this.classList.toggle("active");
+        var panel=this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+        panel.style.maxHeight=panel.scrollHeight + "px";
+    
+    }
+  });
+}
